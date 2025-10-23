4d9333bec8a15d6ff72d78540a2457e43e57f30b
Lucas Marcolongo <lucas_marco@live.com>
2025-10-22 20:01:27 -0700

    fix(role/common): always derive sops key from ssh

--- roles/common.nix
+++ roles/common.nix
@@ -80,8 +80,6 @@ lib.mkIf (lib.elem "common" config.roles) {
   sops = {
     age = {
       sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-      keyFile = "/var/lib/sops-nix/key.txt";
-      generateKey = true;
     };
 
     defaultSopsFormat = "yaml";
