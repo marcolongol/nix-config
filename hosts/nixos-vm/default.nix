6f8a9c15f8f6162f6ef15d6c959b0ed2cc51310f
Lucas Marcolongo <lucas_marco@live.com>
2025-11-13 16:47:34 -0800

    fix(hosts/nixos-vm): add minimal boot and filesystem config to pass flake checks

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -13,6 +13,21 @@ in {
 
   services.qemuGuest.enable = true;
 
+  # Basic filesystem configuration for VM
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+  };
+
+  # Boot loader configuration for VM
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   # Set empty passwords for all normal users in the VM for easy initial access
   # INFO: This is needed because the VM does not have a way to decrypt the user passwords from
   # SOPS due to lack of SSH keys during vm build time.
