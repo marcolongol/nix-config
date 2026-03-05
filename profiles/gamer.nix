576cbc18c526a70e7a35b11243bbe883858ecc15
Lucas Marcolongo <lucas_marco@live.com>
2026-03-04 16:42:29 -0800

    feat(profiles/gamer): add mangohud

--- profiles/gamer.nix
+++ profiles/gamer.nix
@@ -7,6 +7,26 @@
   ...
 }:
 lib.mkIf (lib.elem "gamer" config.profiles) {
+  programs.mangohud = {
+    enable = true;
+    settings = {
+      position = "top-left";
+      toggle_hud = "Shift_R+F12";
+
+      fps = true;
+      frame_timing = 1;
+
+      gpu_stats = true;
+      gpu_temp = true;
+      gpu_power = true;
+      vram = true;
+
+      cpu_stats = true;
+      cpu_temp = true;
+      ram = true;
+    };
+  };
+
   home.packages = with pkgs; [
     discord
   ];
