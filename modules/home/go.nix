4f20fe51ebfedbf0da0a6bc5c95eea5bcc048eff
Lucas Marcolongo <lucas@marcolongo.dev>
2026-06-14 21:24:39 -0400

    feat(home): add Go developer toolchain module

---
+++ modules/home/go.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: lib.mkIf config.profiles.developer.enable {
+  home.packages = with pkgs; [
+    go
+    gopls # LSP server
+    delve # debugger (dlv)
+    (lib.lowPrio gotools) # goimports, etc.; lowPrio so sox wins `play` collision
+    go-tools # staticcheck
+    golangci-lint # meta-linter
+    gomodifytags
+    gotests
+    impl
+  ];
+
+  home.sessionVariables = {
+    GOPATH = "${config.home.homeDirectory}/.local/share/go";
+    GOBIN = "${config.home.homeDirectory}/.local/share/go/bin";
+    GOTELEMETRY = "off";
+  };
+
+  home.sessionPath = ["${config.home.homeDirectory}/.local/share/go/bin"];
+
+  persistentFolders = [
+    ".local/share/go" # GOPATH: modules, installed bins
+    ".cache/go-build" # build cache
+  ];
+}
