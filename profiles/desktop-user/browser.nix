738e14cdbdb23d512714618f25922cbf99756c39
Lucas Marcolongo <lucas@marcolongo.dev>
2026-07-22 09:52:48 -0400

    feat(desktop): update mime handler from zen-browser to zen-beta desktop file
    Enable xdg.mimeApps and point default browser associations at the
    zen-beta.desktop entry provided by the current Zen build.

--- profiles/desktop-user/browser.nix
+++ profiles/desktop-user/browser.nix
@@ -28,13 +28,14 @@ lib.mkIf config.profiles.desktopUser.enable {
 
   stylix.targets.zen-browser.profileNames = ["default"];
 
+  xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
-    "text/html" = "zen-browser.desktop";
-    "text/xml" = "zen-browser.desktop";
-    "application/xhtml+xml" = "zen-browser.desktop";
-    "application/vnd.mozilla.xul+xml" = "zen-browser.desktop";
-    "x-scheme-handler/http" = "zen-browser.desktop";
-    "x-scheme-handler/https" = "zen-browser.desktop";
+    "text/html" = "zen-beta.desktop";
+    "text/xml" = "zen-beta.desktop";
+    "application/xhtml+xml" = "zen-beta.desktop";
+    "application/vnd.mozilla.xul+xml" = "zen-beta.desktop";
+    "x-scheme-handler/http" = "zen-beta.desktop";
+    "x-scheme-handler/https" = "zen-beta.desktop";
   };
 
   persistentFolders = [
